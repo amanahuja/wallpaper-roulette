0c68a2ed6b22cb283d48440201bc1da6007e3b78
Aman Ahuja <amanahuja@gmail.com>
2018-10-04 16:50:46 -0700

    changes to py3

--- set_wallpaper.py
+++ set_wallpaper.py
@@ -1,34 +1,138 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
-# Set wallpapers from reddit
-
-source:
-    http://www.reddit.com/r/wallpaper/.json
-
-1. Get image from reddit:
-2. Download and move
-3. Set image
-4. cleanup / remove old wallpaper?
-
-TBD:
-    log source and filename to help track where awesome images came from.
+Takes care of interfaces to reddit, IG and other sources
+Fetches wallpapers from where specified in the file
 """
 
 import os
-import argparse
-import logging
 import requests
-import numpy as np
 import yaml
+import numpy as np
 
-ROOT_PATH = '/home/aman/wallpaper-roulette'
-DESTINATION_PATH = '/home/aman/Downloads/reddit_wallpaper'
-LOGFILENAME = 'wallpaper_history.log'
-SOURCEFILENAME = 'sources.yaml'
 
-def reddit_post_usable(post):
+class ImageFetcher: 
+    """Fetches images"""
+    def __call__(self, source_url: str, target_dir: str) -> None:
+        header = {
+            'User-Agent': 'n00b wallpaper bot v0.4',
+            # 'Authorization': 'Client-ID ' + client_id
+            }
+        i = requests.get(source_url, stream=True, headers=header)
+
+        img_file_suffix = source_url.split('.')[-1]
+        img_file_suffix = img_file_suffix.split('?')[0]
+
+        dest_path = "{}/{}.{}".format(
+            target_dir,
+            'roulette_background',
+            str(img_file_suffix))
+
+        with open(dest_path, 'wb') as outfile:
+            for chunk in i.iter_content(chunk_size=1024):
+                outfile.write(chunk)
+        i.raise_for_status()
+
+        # TODO: log image source and destination
+
+        return None
+
+class RouletteConfig:
+    """Reads Roulette config file"""
+    def __init__(self, config_path: str = 'sources.yaml') -> None:
+        assert os.path.exists(config_path)
+
+        with open(config_path) as infile:
+            config = yaml.safe_load(infile)
+
+        self.sources = config['Main']
+        self.nsfw_sources = config['NSFW']
+
+    def source_list(self, nsfw: bool = False):
+        """get list of sources"""
+        if nsfw:
+            return self.nsfw_sources
+        return self.sources
+
+    def random_source(self, nsfw: bool = False) -> str:
+        """select a random source from config"""
+        source_list = self.sources
+        if nsfw:
+            # include nsfw sources in random selection
+            source_list = source_list + self.nsfw_sources
+
+        return np.random.choice(source_list)
+
+
+class RouletteSource:
+    """
+    struct and operations on a source for wallpapers
+    """
+    SOURCE_TYPES = [
+        'REDDIT',
+        'INSTAGRAM',
+        'URL']
+
+    def __init__(self, source_type: str, url: str) -> None:
+        assert source_type in RouletteSource.SOURCE_TYPES
+        self.source_type = source_type
+        self.source_url = url
+        return None
+
+    def random_image(self) -> str:
+        """
+        Get specific image url from source
+        method depends on source type
+        """
+        if self.source_type == 'REDDIT':
+            image_source_url = self._image_from_reddit()
+        if self.source_type == 'INSTAGRAM':
+            image_source_url = self._image_from_instagram()
+        if self.source_type == 'URL':
+            image_source_url = self._image_from_direct_url()
+
+        return image_source_url
+
+    def _image_from_reddit(self) -> str:
+        """use a URL for a reddit source to select an image"""
+        assert self.source_type == 'REDDIT'
+
+        source_url = self.source_url
+
+        header = {
+            'User-Agent': 'n00b wallpaper bot v0.4',
+            # 'Authorization': 'Client-ID ' + client_id
+            }
+
+        req = requests.get(source_url, headers=header)
+
+        # Get json from URL
+        json_object = req.json()
+
+        # Get all posts from JSON object
+        posts = json_object['data']['children']
+
+        filtered_posts = [p for p in posts if _check_reddit_post(p)]
+        if not filtered_posts:
+            # logging.warning('[source: "%s"]: no usable posts.', source)
+            exit()
+        selected_post = np.random.choice(filtered_posts)
+
+        image_url = selected_post['data']['url']
+
+        # TODO: logging here
+        # subreddit = selected_post['data']['subreddit']
+        # name = selected_post['data']['title']
+        # logging.info('[subreddit: "%s"]: %s', subreddit, name)
+        # logging.info('[x of %i options]: URL: %s', len(filtered_posts), url)
+
+        return image_url
+
+    def _image_from_instagram(self):
+        raise NotImplementedError
+
+    def _image_from_direct_url(self):
+        raise NotImplementedError
+
+def _check_reddit_post(post):
     """ Return True is reddit post has usable image"""
 
     # Filter out urls that are not usable images
@@ -36,117 +140,20 @@ def reddit_post_usable(post):
     has_suffix = lambda url: any([s in url for s in suffixes])
 
     image_url = post['data']['url']
-    return has_suffix(image_url)
 
-def get_image_url_from_reddit(source):
-    """ Get image URL from a reddit source
-    """
-    header = {
-        'User-Agent': 'n00b wallpaper bot v0.4',
-        # 'Authorization': 'Client-ID ' + client_id
-        }
+    if has_suffix(image_url):
+        return True
+    return False
 
-    req = requests.get(source, headers=header)
+# this is a test
+rconf = RouletteConfig()
+source = rconf.random_source()
+print(source)
 
-    # Get json from URL
-    json_object = req.json()
+rsource = RouletteSource('REDDIT', source)
+img_url = rsource.random_image()
+print(img_url)
 
-    # Get all posts from JSON object
-    posts = json_object['data']['children']
-
-    filtered_posts = [p for p in posts if reddit_post_usable(p)]
-    if not filtered_posts:
-        logging.warning('[source: "%s"]: no usable posts.', source)
-        exit()
-    selected_post = np.random.choice(filtered_posts)
-
-    subreddit = selected_post['data']['subreddit']
-    name = selected_post['data']['title']
-    url = selected_post['data']['url']
-
-    logging.info('[subreddit: "%s"]: %s', subreddit, name)
-    logging.info('[x of %i options]: URL: %s', len(filtered_posts), url)
-
-    return url
-
-def download_image_from_url(url, DESTINATION_PATH):
-    """ Download image from URL
-    """
-
-    image_type = url.split('.')[-1]
-    image_type = image_type.split('?')[0]
-
-    header = {
-        'User-Agent': 'n00b wallpaper bot v0.4',
-        # 'Authorization': 'Client-ID ' + client_id
-        }
-    i = requests.get(url, stream=True, headers=header)
-
-    image_path = "{}.{}".format(DESTINATION_PATH, str(image_type))
-
-    with open(image_path, 'wb') as outfile:
-        for chunk in i.iter_content(chunk_size=1024):
-            outfile.write(chunk)
-    i.raise_for_status()
-
-    return outfile
-
-def main(sources=source_list):
-    """Main function
-    """
-    source = np.random.choice(sources)
-    # print "Using source: {}".format(source)
-
-    url = get_image_url_from_reddit(source)
-    outfile = download_image_from_url(url, DESTINATION_PATH)
-
-    # print "{} --> {}".format(url, outfile)
-
-    setbg_command = "gsettings set org.gnome.desktop.background picture-uri"
-    img_path = "file://{}".format(outfile)
-
-    # import subprocess
-    # subprocess.call([setbg_command, img_path])
-
-    os.system("{} {}".format(setbg_command, img_path))
-
-    return True
-
-def get_source_list(nsfw=False):
-    """Fetches source list from YAML file"""
-
-    source_path = os.path.join(ROOT_PATH, SOURCEFILENAME)
-    with open(source_path) as infile:
-        sources = yaml.safe_load(infile)
-
-    # Pick source list
-    if nsfw:
-        return sources['NSFW']
-    return sources['Main']
-
-
-if __name__ == "__main__":
-    # Parse Args
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        "--nsfw",
-        dest='use_nsfw',
-        action='store_true',
-        help='get image from alternate list of sources'
-        )
-    parser.set_defaults(use_nsfw=False)
-
-    args = parser.parse_args()
-
-    source_list = get_source_list(args.use_nsfw)
-
-    logging.basicConfig(format='%(asctime)s %(message)s',
-                        datefmt='%m/%d/%Y %I:%M:%S %p',
-                        filename=os.path.join(ROOT_PATH, LOGFILENAME),
-                        level=logging.INFO)
-
-    logging.getLogger("requests").setLevel(logging.WARNING)
-
-    # Start
-    main(source_list)
+get_image = ImageFetcher()
+get_image(source_url = img_url, 
+         target_dir = '/home/aman/Downloads/')
---
+++ set_wallpaper_py2.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+# Set wallpapers from reddit
+
+source:
+    http://www.reddit.com/r/wallpaper/.json
+
+1. Get image from reddit:
+2. Download and move
+3. Set image
+4. cleanup / remove old wallpaper?
+
+TBD:
+    log source and filename to help track where awesome images came from.
+"""
+
+import os
+import argparse
+import logging
+import requests
+import numpy as np
+import yaml
+
+ROOT_PATH = '/home/aman/wallpaper-roulette'
+DESTINATION_PATH = '/home/aman/Downloads/reddit_wallpaper'
+LOGFILENAME = 'wallpaper_history.log'
+SOURCEFILENAME = 'sources.yaml'
+
+def reddit_post_usable(post):
+    """ Return True is reddit post has usable image"""
+
+    # Filter out urls that are not usable images
+    suffixes = ['.png', '.jpg']
+    has_suffix = lambda url: any([s in url for s in suffixes])
+
+    image_url = post['data']['url']
+    return has_suffix(image_url)
+
+def get_image_url_from_reddit(source):
+    """ Get image URL from a reddit source
+    """
+    header = {
+        'User-Agent': 'n00b wallpaper bot v0.4',
+        # 'Authorization': 'Client-ID ' + client_id
+        }
+
+    req = requests.get(source, headers=header)
+
+    # Get json from URL
+    json_object = req.json()
+
+    # Get all posts from JSON object
+    posts = json_object['data']['children']
+
+    filtered_posts = [p for p in posts if reddit_post_usable(p)]
+    if not filtered_posts:
+        logging.warning('[source: "%s"]: no usable posts.', source)
+        exit()
+    selected_post = np.random.choice(filtered_posts)
+
+    subreddit = selected_post['data']['subreddit']
+    name = selected_post['data']['title']
+    url = selected_post['data']['url']
+
+    logging.info('[subreddit: "%s"]: %s', subreddit, name)
+    logging.info('[x of %i options]: URL: %s', len(filtered_posts), url)
+
+    return url
+
+def download_image_from_url(url, DESTINATION_PATH):
+    """ Download image from URL
+    """
+
+    image_type = url.split('.')[-1]
+    image_type = image_type.split('?')[0]
+
+    header = {
+        'User-Agent': 'n00b wallpaper bot v0.4',
+        # 'Authorization': 'Client-ID ' + client_id
+        }
+    i = requests.get(url, stream=True, headers=header)
+
+    image_path = "{}.{}".format(DESTINATION_PATH, str(image_type))
+
+    with open(image_path, 'wb') as outfile:
+        for chunk in i.iter_content(chunk_size=1024):
+            outfile.write(chunk)
+    i.raise_for_status()
+
+    return outfile
+
+def main(sources=source_list):
+    """Main function
+    """
+    source = np.random.choice(sources)
+    # print "Using source: {}".format(source)
+
+    url = get_image_url_from_reddit(source)
+    outfile = download_image_from_url(url, DESTINATION_PATH)
+
+    # print "{} --> {}".format(url, outfile)
+
+    setbg_command = "gsettings set org.gnome.desktop.background picture-uri"
+    img_path = "file://{}".format(outfile)
+
+    # import subprocess
+    # subprocess.call([setbg_command, img_path])
+
+    os.system("{} {}".format(setbg_command, img_path))
+
+    return True
+
+def get_source_list(nsfw=False):
+    """Fetches source list from YAML file"""
+
+    source_path = os.path.join(ROOT_PATH, SOURCEFILENAME)
+    with open(source_path) as infile:
+        sources = yaml.safe_load(infile)
+
+    # Pick source list
+    if nsfw:
+        return sources['NSFW']
+    return sources['Main']
+
+
+if __name__ == "__main__":
+    # Parse Args
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--nsfw",
+        dest='use_nsfw',
+        action='store_true',
+        help='get image from alternate list of sources'
+        )
+    parser.set_defaults(use_nsfw=False)
+
+    args = parser.parse_args()
+
+    source_list = get_source_list(args.use_nsfw)
+
+    logging.basicConfig(format='%(asctime)s %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p',
+                        filename=os.path.join(ROOT_PATH, LOGFILENAME),
+                        level=logging.INFO)
+
+    logging.getLogger("requests").setLevel(logging.WARNING)
+
+    # Start
+    main(source_list)
